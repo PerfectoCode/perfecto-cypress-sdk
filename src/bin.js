2a065765bc85d61bbc3deb9d0d2166f0a2565052
Elhay Avihezer <elhayav@gmail.com>
2020-08-23 23:12:22 +0300

    config updates

--- src/bin.js
+++ src/bin.js
@@ -7,7 +7,8 @@ yargs
   .demandCommand(1, 'You need at least one command before moving on')
 
   .commandDir('cmds', {extensions: ['cmd.js']})
-
+  .strictCommands()
+  .showHelpOnFail(false, 'Specify --help for available options')
   .version()
   .help()
   .argv;
