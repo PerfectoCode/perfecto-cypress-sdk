08ac32a1fdeb5ad26119d61e05e4111c5ebb0bec
Elhay Avihezer <elhayav@gmail.com>
2020-08-23 09:57:03 +0300

    yargs configuration - cancel camel-case-expansion

--- src/bin.js
+++ src/bin.js
@@ -3,6 +3,7 @@ import yargs from 'yargs';
 
 // noinspection BadExpressionStatementJS
 yargs
+  .parserConfiguration({'camel-case-expansion': false})
   .demandCommand(1, 'You need at least one command before moving on')
 
   .commandDir('cmds', {extensions: ['cmd.js']})
