9e4f6a094141d77c70f3425601be4f41c7885cce
Elhay Avihezer <elhayav@gmail.com>
2020-09-02 00:13:31 +0300

    Pack unit test and fixes

---
+++ test/pack.spec.js
@@ -0,0 +1,70 @@
+import path from 'path';
+import fs from 'fs';
+import AdmZip from 'adm-zip';
+import { expect } from 'chai';
+
+import packCommand from '../src/pack';
+import { DEFAULT_ARCHIVE_FILE_NAME } from '../src/common/defaults';
+
+describe('Pack', () => {
+  let outPath;
+  const createUniqueOutPath = () => 'test/temp/'+ new Date().getTime();
+  afterEach(() => {
+    if (!outPath) {
+      return;
+    }
+    fs.unlinkSync(path.resolve(outPath, DEFAULT_ARCHIVE_FILE_NAME));
+    fs.rmdir(path.resolve(outPath), (error) => {
+      if (error) {
+        console.error('Clean failed:', error);
+      }
+    });
+    outPath = null;
+  });
+
+  it('should create outPath if not exist', async () => {
+    outPath = createUniqueOutPath();
+    await packCommand('test/resources/archive-files/', [], outPath);
+    expect(fs.existsSync(outPath), 'outPath: ' + outPath + ', not exist').to.be.true;
+  });
+
+  it('should include all files without predefined ignored files', async () => {
+    outPath = createUniqueOutPath();
+    await packCommand('test/resources/archive-files/', [], outPath);
+
+    const admZip = new AdmZip(path.resolve(outPath, DEFAULT_ARCHIVE_FILE_NAME));
+    const zipFiles = admZip.getEntries().map(i => i.entryName);
+
+    expect(zipFiles).to.not.contain('node_modules/excluded-by-default.txt');
+    expect(zipFiles).to.not.contain('perfecto-cypress.zip');
+  });
+
+  it('should exclude ignoredRegexList files', async () => {
+    outPath = createUniqueOutPath();
+    await packCommand('test/resources/archive-files/', ['test-file[1-9].text'], outPath);
+
+    const admZip = new AdmZip(path.resolve(outPath, DEFAULT_ARCHIVE_FILE_NAME));
+    const zipFiles = admZip.getEntries().map(i => i.entryName);
+
+    expect(zipFiles).to.not.contain('test-file2.text');
+  });
+
+  it('should throw exception if testRoot is an empty folder', async () => {
+    try {
+      await packCommand('test/resources/empty', [], 'test/temp');
+      expect.fail('should throw exception');
+    } catch (error) {
+      expect(error.message).contain('Zip archive contain zero files');
+    }
+  });
+
+  it('should throw exception if archive is empty', async () => {
+    outPath = createUniqueOutPath();
+    try {
+      await packCommand('test/resources/archive-files', ['**/**'], outPath);
+      expect.fail('should throw exception');
+    } catch (error) {
+      expect(error.message).contain('Zip archive contain zero files');
+    }
+  });
+});
--- test/util/setup.js
+++ test/util/setup.js
@@ -1,5 +1,7 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiArrays from 'chai-arrays'
 
 chai.should();
 chai.use(sinonChai);
+chai.use(chaiArrays);
