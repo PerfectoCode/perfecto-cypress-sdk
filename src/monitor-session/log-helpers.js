fd5a21889be148158488fb0c06cd427336cc5a6b
Elhay Avihezer <elhayav@gmail.com>
2020-10-13 03:01:18 +0300

    fix platform parsing

--- src/monitor-session/log-helpers.js
+++ src/monitor-session/log-helpers.js
@@ -7,7 +7,7 @@ export const truncate = (text, length) => {
   return text.slice(0, length) + '...';
 };
 export const objectToHash = data => {
-  if (data) {
+  if (!data) {
     return 'N/A';
   }
   return Object.values(data).map(item => typeof item === 'object' ? objectToHash(item) : item ).filter(item => item).join('-');
