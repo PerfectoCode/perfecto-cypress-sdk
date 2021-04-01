ebe4e4b8b276a4bf32f1b27441c29331193a6d55
nashati <nashati@perfectomobile.com>
2021-04-01 10:55:19 +0300

    add auto import for reporter

--- src/cmds/init.cmd.js
+++ src/cmds/init.cmd.js
@@ -10,6 +10,7 @@ export const builder = {
   prompt: { default: true, hidden: true, boolean: true },
   'tests.path': { ...testsOptions['tests.path'] },
   ...credentialsOptions,
+  "add-reporter" : {alias:'ar', type: 'boolean', default: true, describe: 'Add perfecto-cypress-reporter automatically'},
   skip: { alias: 's', type: 'boolean', default: false, describe: 'Skip interactive questions' },
 };
 
@@ -18,15 +19,16 @@ const getInitOptions = (argv) => {
     cloud: { type: 'input', default: argv.credentials?.cloud, describe: 'Enter Perfecto cloud name' },
     securityToken : {type: 'input', default: argv.credentials?.securityToken, describe: 'Enter your Perfecto security token'},
     testsPath: { type: 'input', default: argv.tests?.path || './', describe: 'Enter path for cypress folder', ...argv.tests?.path ? {} : {prompt: 'always'} },
+    'add-reporter': { type: 'input', default: argv['add-reporter'], describe: 'Add perfecto-cypress-reporter automatically?', prompt: 'always' },
   };
 }
 
 export const handler = async (argv) => {
   if (argv.skip) {
-    initCommand(argv.credentials?.cloud, argv.credentials?.securityToken, argv.tests?.path);
+    initCommand(argv.credentials?.cloud, argv.credentials?.securityToken, argv.tests?.path, argv['add-reporter']);
   } else {
     const options = getInitOptions(argv);
     const result = await yargsInteractive().interactive({ ...options, interactive: { default: argv.prompt } });
-    initCommand(result.cloud, result.securityToken, result.testsPath);
+    initCommand(result.cloud, result.securityToken, result.testsPath, result['add-reporter']);
   }
 };
